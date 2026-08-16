697e3e9054fd90a95adfd2dafa2a3f8a165382a1
evexus <inthisonejayvee@gmail.com>
2026-08-16 13:31:19 -0700

    Initial project setup with player lookup

---
+++ client/src/App.tsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+
+// Outline data specifically expect certain types for each field
+type PlayerData = {
+  name: string;
+  race: string;
+  class: string;
+  active_spec_name: string;
+  active_spec_role: string;
+  gender: string;
+  faction: string;
+  achievement_points: number;
+  region: string;
+  realm: string;
+  profile_url: string;
+};
+
+/*
+* User enters character and name
+* React stores those values with state
+* User Clicks Get Player -> React sends GET request/Fetch to Espress
+* Express has route to handle this and fetches from raider io 
+* Sends this data that it got from Raider IO to the frontend
+* React displays this data 
+*/
+function App() {
+
+  // SearchName is what the user types into search
+  // playerName is name returned into raider io
+  const [searchName, setSearchName] = useState("");
+  const [searchRealm, setSearchRealm] = useState("");
+  const [error, setError] = useState(""); // Stores current error message 
+
+  // Specify the outline of the object we are using for the data 
+  const [playerData, setPlayerData] = useState<PlayerData | null>(null);
+
+
+  async function getPlayer() {
+    setError("");
+
+    // Fetch is the HTTP request to this url 
+    // Back ticks instead of quotes to insert variable name in our search parameter 
+    const response = await fetch(
+      `http://localhost:3001/api/player/us/${searchRealm}/${searchName}`
+    );
+
+    const data = await response.json();
+
+    // Successful HTTP is stored in the response - Boolean
+    // Exit out and Set Data and Player Name to Cleared Values
+    // Display this on your JSX 
+    if (!response.ok) {
+      setError(data.error || "Unable to find player");
+      setPlayerData(null);
+      return;
+    }
+
+    setPlayerData(data);
+  }
+
+  return (
+    <div>
+      <h1>WoW Player History</h1>
+
+      <input
+        type="text"
+        value={searchName}
+        onChange={(event) => {
+          setSearchName(event.target.value);
+        }}
+        placeholder="Character Name"
+      />
+
+      <input
+        type="text"
+        value={searchRealm}
+        onChange={(event) => {
+          setSearchRealm(event.target.value);
+        }}
+        placeholder="Realm"
+      />
+
+      <button onClick={getPlayer}>
+        Get Player
+      </button>
+      
+      {/*Displays the error message if it does exist - Can still render empty paragraph even if error is NULL*/}
+      {error && <p>{error}</p>}
+      
+      {/*Player data will be set to null if it doesnt exist - Not just dumped as JSON - frontend interpreting and processing it*/}
+      {playerData && (
+        <div>
+          <h2>{playerData.name}</h2>
+
+          <p>Realm: {playerData.realm}</p>
+
+          <p>Class: {playerData.class}</p>
+
+          <p>Spec: {playerData.active_spec_name}</p>
+
+          <p>Role: {playerData.active_spec_role}</p>
+
+          <p>Faction: {playerData.faction}</p>
+
+          <p>Achievement Points: {playerData.achievement_points}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,86 @@
+// Load the installed Express package into the variable `express`.
+// Installing Express makes it available to the project, but does not
+// automatically make it available inside every JavaScript file.
+// `express` now gives this file access to the functionality exported by Express.
+const express = require("express");
+const cors = require("cors");
+ 
+// Call the Express function to create an Express application.
+// Store that application in the variable `app`.
+const app = express();
+
+// Enable CORS for requests
+// JS from one origin doesnt auto get permission to read resources from another origin
+// Allows React frontend to make requests to backend on different ports
+app.use(cors());
+
+// Register a route for GET requests to "/".
+// This tells Express:
+// "If you receive a GET request for /, run this callback function."
+//
+// The callback is NOT executed right now.
+// We are configuring the application before we start accepting requests.
+//
+// `req` represents the incoming HTTP request.
+// `res` represents the HTTP response we will send back.
+app.get("/", (req, res) => {
+
+    // Send an HTTP response back to whoever made the request.
+    res.send("WoW API server is running");
+});
+
+app.get("/api/player/:region/:realm/:name", async (req, res) => {
+
+    // Values that begin with ":" in the route are URL parameters.
+    // Express reads them from the incoming request and puts them in req.params.
+    const { region, realm, name } = req.params;
+
+    // Build the Raider.IO API URL using the character information
+    // that came from our own URL.
+    const url =
+        `https://raider.io/api/v1/characters/profile?region=${region}&realm=${realm}&name=${name}&fields=mythic_plus_scores_by_season:current`;
+
+    // Try running code but if theres an error, send status and do not return data
+    try {
+        // Send a GET request to Raider.IO and wait for the response.
+        const response = await fetch(url);
+
+        // Convert Raider.IO's JSON response body into a JavaScript object.
+        const data = await response.json();
+
+        if (!response.ok) {
+
+            // Send error HTTP status back to our frontend
+            return res.status(response.status).json({
+                error: data.message || "Unable to find player"
+            });
+        }
+
+        // Send that data back to whoever called our API.
+        res.json(data);
+
+    } catch (error) {
+        
+        // Other errors such as unable being able to connect to raider io
+        res.status(500).json({
+            error: "Server error while contacting Raider.IO"
+        });
+    }
+
+});
+
+
+// Our React development server is running on localhost:5173.
+// We will run our backend HTTP server on port 3001.
+//
+// An HTTP server is a program that receives HTTP requests
+// and sends HTTP responses.
+//
+// Start the HTTP server and listen for incoming connections on port 3001.
+// Once the server successfully starts listening, run the callback below.
+app.listen(3001, () => {
+
+    // Print this message to our terminal.
+    // This is only for us, the developer. It is NOT sent to the browser.
+    console.log("Server running on http://localhost:3001");
+});
